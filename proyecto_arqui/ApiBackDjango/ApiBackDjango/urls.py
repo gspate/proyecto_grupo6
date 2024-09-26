d0efc866a192e75538afe0684c6a3e07178d2bc6
Alvaro Bustos <aabm@uc.cl>
2024-09-26 20:17:50 -0300

    Small fix2

--- proyecto_arqui/ApiBackDjango/ApiBackDjango/urls.py
+++ proyecto_arqui/ApiBackDjango/ApiBackDjango/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import LoginView, LogoutView, RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    
 ]
