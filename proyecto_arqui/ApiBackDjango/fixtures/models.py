b4ce1d39f7e98d979260b7b3d3de2bd24efc4ee6
Alvaro Bustos <aabm@uc.cl>
2024-09-26 19:29:15 -0300

    Pasando a django

---
+++ proyecto_arqui/ApiBackDjango/fixtures/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class Fixture(models.Model):
+    fixture_id = models.IntegerField(unique=True)
+    referee = models.CharField(max_length=100, null=True, blank=True)
+    timezone = models.CharField(max_length=50, null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
+    timestamp = models.IntegerField()
+    status_long = models.CharField(max_length=50, null=True, blank=True)
+    status_short = models.CharField(max_length=50, null=True, blank=True)
+    status_elapsed = models.IntegerField(null=True, blank=True)
+    league_id = models.IntegerField(null=True, blank=True)
+    league_name = models.CharField(max_length=100, null=True, blank=True)
+    league_country = models.CharField(max_length=100, null=True, blank=True)
+    league_logo = models.CharField(max_length=255, null=True, blank=True)
+    league_flag = models.CharField(max_length=255, null=True, blank=True)
+    league_season = models.IntegerField(null=True, blank=True)
+    league_round = models.CharField(max_length=100, null=True, blank=True)
+    home_team_id = models.IntegerField(null=True, blank=True)
+    home_team_name = models.CharField(max_length=100, null=True, blank=True)
+    home_team_logo = models.CharField(max_length=255, null=True, blank=True)
+    home_team_winner = models.BooleanField(null=True, blank=True)
+    away_team_id = models.IntegerField(null=True, blank=True)
+    away_team_name = models.CharField(max_length=100, null=True, blank=True)
+    away_team_logo = models.CharField(max_length=255, null=True, blank=True)
+    away_team_winner = models.BooleanField(null=True, blank=True)
+    home_goals = models.IntegerField(null=True, blank=True)
+    away_goals = models.IntegerField(null=True, blank=True)
+    odds_id = models.IntegerField(null=True, blank=True)
+    odds_name = models.CharField(max_length=100, null=True, blank=True)
+    odds_home_value = models.FloatField(null=True, blank=True)
+    odds_draw_value = models.FloatField(null=True, blank=True)
+    odds_away_value = models.FloatField(null=True, blank=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Fixture {self.fixture_id}"
