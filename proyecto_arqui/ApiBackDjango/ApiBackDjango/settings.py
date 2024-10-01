2f64facc3a8ed2b596dd9596d02d7363cc0628b5
Alvaro Bustos <aabm@uc.cl>
2024-10-01 15:39:02 -0300

    Cors generico

--- proyecto_arqui/ApiBackDjango/ApiBackDjango/settings.py
+++ proyecto_arqui/ApiBackDjango/ApiBackDjango/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'fixtures',
     'accounts',
+    'corsheaders',
 ]
+CORS_ALLOW_ALL_ORIGINS = True #Ver bien esto, es suplementario por mientras
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
