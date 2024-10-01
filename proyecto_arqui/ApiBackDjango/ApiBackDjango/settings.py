721ab838855f163c52638b9388c17edbf4a86722
Álvaro_Sotomayor <112011296+AlvaroSotomayor@users.noreply.github.com>
2024-10-01 18:38:27 -0300

    Update settings.py

--- proyecto_arqui/ApiBackDjango/ApiBackDjango/settings.py
+++ proyecto_arqui/ApiBackDjango/ApiBackDjango/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Auth.middleware.token_required',
+    'ApiBackDjango.middleware.jwt_required',
 ]
 
 ROOT_URLCONF = 'ApiBackDjango.urls'
