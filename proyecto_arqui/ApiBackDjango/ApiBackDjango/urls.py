d0adbffd9550ffb4d3e96e0ffe42c1009eec3b71
Álvaro_Sotomayor <112011296+AlvaroSotomayor@users.noreply.github.com>
2024-10-01 18:16:28 -0300

    Update urls.py

--- proyecto_arqui/ApiBackDjango/ApiBackDjango/urls.py
+++ proyecto_arqui/ApiBackDjango/ApiBackDjango/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
+from django.contrib import admin, auth
 from django.urls import path, re_path
 from wallet.views import WalletInfoView
 from fixtures.views import FixtureList, FixtureDetail, BonusRequestView, BonusValidationView
